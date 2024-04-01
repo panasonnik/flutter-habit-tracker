516798e57ddfbf15057fa039137d95f2df94047d
panasonikk <grebenyuk1507@gmail.com>
2024-04-01 10:16:35 +0200

    Initial commit, imported fonts

---
+++ habit_tracker/lib/widgets/app_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}
